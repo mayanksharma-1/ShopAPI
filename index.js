3b8b2c8eb4dd3ecb1ce32523d12c3a857aa60922
Mayank Sharma <as.mayanksharma@gmail.com>
2024-08-14 04:03:43 +0530

    configure middleware to receive json body in the post request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+app.use(express.json());
 require("dotenv").config();
 
 app.listen(3000,'localhost',()=>{
